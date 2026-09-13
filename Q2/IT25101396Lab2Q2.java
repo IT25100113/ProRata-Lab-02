c318c7dfb181a219cdcccaf8a0933cf3cef30dff
IT25100113 <IT25100113@my.sliit.lk>
2026-09-13 23:00:30 +0530

    Add files via upload

---
+++ Q2/IT25101396Lab2Q2.java
@@ -0,0 +1,10 @@
+public class IT25100113Lab2Q2 {
+       public static void main(String[] args) {
+	   
+	        double length = 10;
+			double perimeter = 4*length;
+			double radius = (perimeter*7)/44;
+			
+			System.out.print("Radius of the circular fence: " + radius);
+		}
+}
