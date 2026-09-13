b0e804ff01e00075cdf22a0f6c9f6f207f37d5c9
IT25100113 <IT25100113@my.sliit.lk>
2026-09-13 22:59:58 +0530

    Add files via upload

---
+++ Q1/IT25100113Lab2Q1.java
@@ -0,0 +1,11 @@
+public class IT25100113Lab2Q1{
+          public static void main(String[]args){
+		  
+		     double perimeter = 100;
+			 double length = (perimeter*4)/14;
+			 double width = (3.0/4)*length;
+			 
+			 System.out.println("Length of the fence: " + length);
+			 System.out.println("width of the fence: " +width);
+		}
+}
